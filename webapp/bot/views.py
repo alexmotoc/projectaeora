a502f73c4c34b8904b25437ded3ba2da09a8735e
Alex Motoc <alexmotoc97@gmail.com>
2018-01-29 11:40:16 +0000

    Added two empty views

--- webapp/bot/views.py
+++ webapp/bot/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def chat(request):
+    return render(request, 'chat.html')
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from bot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('chat/', views.chat, name='chat'),
 ]

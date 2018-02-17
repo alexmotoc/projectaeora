53b9b3d4701e728f13f227b045898d489b4f1e80
alexmcfarlane97 <alexmcfarlane97@gmail.com>
2018-02-17 15:54:54 +0000

    Fixed sorting of sector data
    Remove commas from data and convert to float before sorting to remove sorting errors and modified a couple of variable names to match Dialogflow

--- scrapper/footsie/Sector.py
+++ scrapper/footsie/Sector.py
@@ -5,8 +5,8 @@ class Sector:
     def __init__(self, name):
         self.name = name
         self.companies = list()
-        self.risers = list()
-        self.fallers = list()
+        self.rising = list()
+        self.falling = list()
         self.news = list()
         self.highest_price = None
         self.lowest_price = None
@@ -14,21 +14,21 @@ class Sector:
     def add_company(self, company):
         self.companies.append(company)
         if "+" in company.stock.per_diff:
-            self.risers.append(company)
-            self.risers.sort(key=lambda x: x.stock.per_diff, reverse=True)
+            self.rising.append(company)
+            self.rising.sort(key=lambda x: float(x.stock.per_diff[1:]), reverse=True)
         else:
-            self.fallers.append(company)
-            self.fallers.sort(key=lambda x: x.stock.per_diff, reverse=True)
+            self.falling.append(company)
+            self.falling.sort(key=lambda x: float(x.stock.per_diff[1:]), reverse=True)
         for n in company.news:
             self.news.append(n)
         self.news.sort(key=lambda x: datetime.strptime(x.date, '%H:%M %d-%b-%Y'), reverse=True) #latest article first
         if self.highest_price == None:
             self.highest_price = company
         else:
-            if company.stock.price > self.highest_price.stock.price:
+            if float(company.stock.price.replace(",","")) > float(self.highest_price.stock.price.replace(",","")):
                 self.highest_price = company
         if self.lowest_price == None:
             self.lowest_price = company
         else:
-            if company.stock.price < self.lowest_price.stock.price:
+            if float(company.stock.price.replace(",","")) < float(self.lowest_price.stock.price.replace(",","")):
                 self.lowest_price = company

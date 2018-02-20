d1a93388e7bb9e584b8b5e807e2e108252e191be
Steven Pewsey <Stevenpewsey@gmail.com>
2018-02-20 22:13:16 +0000

    Create basic IndexView tests

--- webapp/bot/tests/test_views.py
+++ webapp/bot/tests/test_views.py
@@ -1,5 +1,24 @@
+from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from bot.views import index
+
+class IndexViewTests(TestCase):
+
+    def test_root_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_index_view_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+
+        # TODO: Fill this in once the landing page has been created.
+        # Until then fail this test.
+        self.fail("Implement the landing page")
 
 class ChatViewTests(TestCase):
     def test_chat_view_loads(self):

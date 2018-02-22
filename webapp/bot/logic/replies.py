d623a0c156ca0bafc93f17d308fc5120efc56565
Steven Pewsey <stevenpewsey@gmail.com>
2018-02-22 21:45:50 +0000

    Remove need for scraper in news_reply

--- webapp/bot/logic/replies.py
+++ webapp/bot/logic/replies.py
@@ -1,22 +1,14 @@
 import json
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../../../scraper'))
-
-from footsie import Scraper
-
-scraper = Scraper.Scraper()
 
 
 def obj_dict(obj):
     return obj.__dict__
 
 
-def news_reply(company_code):
+def news_reply(lse_list, yahoo_list):
     overall_dict = {
-        "London Stock Exchange": scraper.get_financial_news_data(company_code),
-        "Yahoo News": scraper.get_yahoo_news_data(company_code)
+        "London Stock Exchange": lse_list,
+        "Yahoo News": yahoo_list
                     }
 
     output_json = json.dumps(overall_dict, default=lambda o: o.__dict__, indent=4)

6b6bca551f1c9808b3cefe83b09c88fbd2f8ec06
Steven Pewsey <stevenpewsey@gmail.com>
2018-02-22 20:59:19 +0000

    Tidy up news_reply method

--- webapp/bot/logic/replies.py
+++ webapp/bot/logic/replies.py
@@ -1,30 +1,24 @@
-import os
-
-import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../../../scraper'))
-
 import json
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../../../scraper'))
 
 from footsie import Scraper
 
 scraper = Scraper.Scraper()
 
+
 def obj_dict(obj):
     return obj.__dict__
 
+
 def news_reply(company_code):
-    overall_dict = {}
+    overall_dict = {
+        "London Stock Exchange": scraper.get_financial_news_data(company_code),
+        "Yahoo News": scraper.get_yahoo_news_data(company_code)
+                    }
 
-    overall_dict["London Stock Exchange"] = scraper.get_financial_news_data(company_code)
+    output_json = json.dumps(overall_dict, default=lambda o: o.__dict__, indent=4)
 
-    overall_dict["Yahoo News"] = scraper.get_yahoo_news_data(company_code)
-
-
-
-    output_json = json.dumps(overall_dict, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
-    print(output_json)
-
-
-
-news_reply("BARC")
+    return output_json

07839abec5385f75077091e9076a4b9f36d2ff2f
Steven Pewsey <stevenpewsey@gmail.com>
2018-02-23 12:42:38 +0000

    Add text parameter in news_reply JSON. Remove method that is no longer needed.

--- webapp/bot/logic/replies.py
+++ webapp/bot/logic/replies.py
@@ -46,17 +46,14 @@ def big_movers_card(top5, risers=True):
 
     return json.dumps(big_movers)
 
-def obj_dict(obj):
-    return obj.__dict__
 
 def news_reply(lse_list, yahoo_list):
+
+    news = {"LSE": lse_list, "YAHOO": yahoo_list}
     overall_dict = {
         "speech": "Here are some news articles that I've found!",
         "type": "news",
-        "London Stock Exchange": lse_list,
-        "Yahoo News": yahoo_list
+        "text": news
     }
 
-    output_json = json.dumps(overall_dict, default=lambda o: o.__dict__, indent=4)
-
-    return output_json
+    return json.dumps(overall_dict, default=lambda o: o.__dict__, indent=4)

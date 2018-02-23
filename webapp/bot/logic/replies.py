fbd94ef357699a6abf9aba977ca1c698b9732ae0
alexmcfarlane97 <alexmcfarlane97@gmail.com>
2018-02-23 15:58:35 +0000

    Add company_reply function to replies

--- webapp/bot/logic/replies.py
+++ webapp/bot/logic/replies.py
@@ -57,3 +57,26 @@ def news_reply(lse_list, yahoo_list):
     }
 
     return json.dumps(overall_dict, default=lambda o: o.__dict__, indent=4)
+
+def get_company_reply(company, attribute):
+    try:
+        value = getattr(company.stock, attribute)
+    except AttributeError:
+        value = getattr(company, attribute)
+    #related_attribute determines what related data will appear to complement the requested data
+    related_attribute = {"bid": "offer", "offer": "bid", "sector": "sub_sector"
+    , "sub_sector": "sector", "high": "low", "low" : "high", "diff": "per_diff"
+    , "per_diff": "diff",  "last_close_value": "last_close_date"
+    ,"last_close_date": "last_close_value", "revenue": "market_cap"
+    ,"market_cap": "revenue", "volume" : "price", "price": "per_diff"}
+    #to_english determines the english word that will be substituted for the attribute name
+    to_english = {"bid": "bid", "offer": "offer", "sector": "sector", "sub_sector": "sub-sector", "high": "high", "low": "low", "diff": "change", "per_diff": "percentage change",
+    "last_close_value": "last close", "last_close_date": "last close date", "revenue": "revenue", "market_cap": "market cap", "volume": "volume", "price": "price"}
+    secondary_attribute = related_attribute[attribute]
+    try:
+        secondary_value = getattr(company.stock, secondary_attribute)
+    except AttributeError:
+        secondary_value = getattr(company, secondary_attribute)
+    speech = "The "+to_english[attribute]+" of "+company.name+" is "+ value #The text to be spoken by the agent
+    return str(json.dumps({'speech': speech,'text': {'name' : company.name,'code': company.code,'date': company.date,'primary': value,
+    'secondary': secondary_value,'primary_type': attribute, 'secondary_type': secondary_attribute},"type": "company"}))

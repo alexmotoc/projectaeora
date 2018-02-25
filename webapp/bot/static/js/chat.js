c4fd24513f0a480392a96e42e5a8c942f5d430c8
Alex Motoc <A.Motoc@warwick.ac.uk>
2018-02-25 17:52:27 +0000

    Added scrollable containers for news cards

--- webapp/bot/static/js/chat.js
+++ webapp/bot/static/js/chat.js
@@ -1,4 +1,20 @@
+jQuery(function ($) {
+    $.fn.hScroll = function (amount) {
+        amount = amount || 120;
+        $(this).bind("DOMMouseScroll mousewheel", function (event) {
+            var oEvent = event.originalEvent,
+                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
+                position = $(this).scrollLeft();
+            position += direction > 0 ? -amount : amount;
+            $(this).scrollLeft(position);
+            event.preventDefault();
+        })
+    };
+});
+
 $(document).ready(function() {
+    $('.scrollable-container').hScroll();
+
     $('.tooltipped').tooltip({delay: 50});
 
     var csrftoken = $.cookie('csrftoken');

a1769a2e3b132484de6b1b1d71f8a1d0ce325efe
alexmcfarlane97 <alexmcfarlane97@gmail.com>
2018-02-25 15:59:12 +0000

    Replace sector output with call to replies.sector_reply

--- webapp/bot/logic/intents.py
+++ webapp/bot/logic/intents.py
@@ -41,25 +41,7 @@ def sector_query_intent(r, is_sector):
             sector_name = r['result']['parameters']['subsector']
             sector_attribute = r['result']['parameters']['sector_attribute']
             sector = scraper.get_sub_sector_data(sector_name)
-    data = getattr(sector, sector_attribute)
-    #form response
-    if sector_attribute == "highest_price" or sector_attribute == "lowest_price":
-        return "{} has the {} {} in {}: {}".format(data.name, sector_attribute.split('_',1)[0], sector_attribute.split('_', 1)[1], sector_name, getattr(data.stock, sector_attribute.split('_', 1)[1]))
-    elif sector_attribute == "news":
-        response = ""
-        for n in data:
-            response += str(n)
-        return response
-    elif sector_attribute == "rising" or sector_attribute == "falling":
-        if (len(data)==0):
-            return "No companies in {} are {}".format(sector_name, sector_attribute)
-        else:
-            response = ""
-            for company in data:
-                if len(response) > 0:
-                    response += ","
-                response += "{} is {}: {}%".format(company.name, sector_attribute, company.stock.per_diff)
-            return response
+    return replies.sector_reply(sector, sector_attribute)
 
 def top_risers_intent(r):
     if r['result']['parameters']['rise_fall'] == '':

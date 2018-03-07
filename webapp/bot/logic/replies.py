f4da9ab6455d78c5c59524d01cdae7327be5e3a3
alexmcfarlane97 <alexmcfarlane97@gmail.com>
2018-03-07 12:10:40 +0000

    Add comments to replies.py

--- webapp/bot/logic/replies.py
+++ webapp/bot/logic/replies.py
@@ -6,8 +6,12 @@ import bs4
 import os
 from datetime import datetime, timedelta
 
+
 def get_stopwords():
-    #source=http://xpo6.com/list-of-english-stop-words/
+    """
+    :return: A list of stop words
+    """
+    # source=http://xpo6.com/list-of-english-stop-words/
     filepath = os.path.dirname(__file__) + '/stopwords.txt'
     stopwords = list()
     with open(filepath) as fp:
@@ -17,14 +21,19 @@ def get_stopwords():
             line = fp.readline()
     return stopwords
 
+
 def get_keywords(article):
+    """
+    :param article: A TextBlob of text to find the keywords from
+    :return: A list of keywords from the text
+    """
     stopwords = get_stopwords()
     words = article.words
     non_stopwords = list()
     for word in words:
         if word.lower() not in stopwords:
             non_stopwords.append(word.lower())
-    words_sorted_by_frequency = sorted(non_stopwords,key=non_stopwords.count,reverse=True)
+    words_sorted_by_frequency = sorted(non_stopwords, key=non_stopwords.count, reverse=True)
     keywords = set()
     for word in words_sorted_by_frequency:
         if len(keywords)<5:
@@ -33,11 +42,16 @@ def get_keywords(article):
             break
     return list(keywords)
 
+
 def get_analysis(content):
+    """
+    :param content: A string with the text content to analyse
+    :return: A string specifying the sentiment of the text
+    :return: A list of the keywords in the text
+    """
     blob = TextBlob(content)
     keywords = get_keywords(blob)
 
-    sentiment = None
     if blob.sentiment.polarity > 0:
         sentiment = 'positive'
     elif blob.sentiment.polarity == 0:
@@ -47,19 +61,18 @@ def get_analysis(content):
 
     return sentiment, keywords
 
-#to_english determines the english word that will be substituted for the attribute name
+# to_english determines the english word that will be substituted for the attribute name
 to_english = {"bid": "bid", "offer": "offer", "sector": "sector", "sub_sector": "sub-sector",
-"high": "high", "low": "low", "diff": "change", "per_diff": "percentage change",
-"last_close_value": "last close", "last_close_date": "last close date", "revenue": "revenue",
-"market_cap": "market cap", "volume": "volume", "price": "price"}
+              "high": "high", "low": "low", "diff": "change", "per_diff": "percentage change",
+              "last_close_value": "last close", "last_close_date": "last close date", "revenue": "revenue",
+              "market_cap": "market cap", "volume": "volume", "price": "price"}
+
 
 def big_movers_card(top5, risers=True):
     """
-    Returns a dictionary containing the layout of the big movers card tables.
-
-    Keyword arguments:
-    top5 - a list of tuples containing data about the top 5 companies
-    risers - specified whether the list contains the risers (True) or fallers (False)
+    :param top5: A list of tuples containing data about the top 5 companies
+    :param risers: Specify whether the list contains the risers (True) or fallers (False)
+    :return: A dictionary containing the layout of the big movers card tables.
     """
     big_movers = defaultdict()
 
@@ -97,9 +110,15 @@ def big_movers_card(top5, risers=True):
 
     return big_movers
 
-def news_reply(financial_news, days):
-    reply = defaultdict()
 
+def news_reply(financial_news, days):
+    """
+    :param financial_news: A dictionary containing lists of LSE news and YAHOO news
+    :param days: An integer specifying the number of days of news the response should contain
+    :return: A dictionary containing the layout of the news cards
+    """
+    reply = defaultdict()
+    # filter out LSE news that is too old
     lse_news = []
     for el in financial_news['LSE']:
         date = datetime.strptime(el.date, '%H:%M %d-%b-%Y')
@@ -114,7 +133,7 @@ def news_reply(financial_news, days):
             row["sentiment"] = "none"
             row["keywords"] = list()
             lse_news.append(row)
-
+    # filter out YAHOO news that is too old
     yahoo_news = []
     for i in financial_news['YAHOO']:
         date = datetime.strptime(i.date, '%H:%M %d-%b-%Y')
@@ -148,6 +167,11 @@ def news_reply(financial_news, days):
 
 
 def get_company_reply(company, attribute):
+    """
+    :param company: A Company whose data was requested
+    :param attribute: The attribute that the response should display the value of
+    :return: A dictionary containing the layout of the company card
+    """
     reply = defaultdict()
 
     try:
@@ -155,12 +179,12 @@ def get_company_reply(company, attribute):
     except AttributeError:
         value = getattr(company, attribute)
 
-    #related_attribute determines what related data will appear to complement the requested data
-    related_attribute = {"bid": "offer", "offer": "bid", "sector": "sub_sector"
-    , "sub_sector": "sector", "high": "low", "low" : "high", "diff": "per_diff"
-    , "per_diff": "diff",  "last_close_value": "last_close_date"
-    ,"last_close_date": "last_close_value", "revenue": "market_cap"
-    ,"market_cap": "volume", "volume" : "price", "price": "per_diff"}
+    # related_attribute determines what related data will appear to complement the requested data
+    related_attribute = {"bid": "offer", "offer": "bid", "sector": "sub_sector",
+                         "sub_sector": "sector", "high": "low", "low" : "high", "diff": "per_diff",
+                         "per_diff": "diff",  "last_close_value": "last_close_date",
+                         "last_close_date": "last_close_value", "revenue": "market_cap",
+                         "market_cap": "volume", "volume" : "price", "price": "per_diff"}
 
     secondary_attribute = related_attribute[attribute]
 
@@ -170,23 +194,29 @@ def get_company_reply(company, attribute):
         secondary_value = getattr(company, secondary_attribute)
 
     card = {'name' : company.name,'code': company.code,'date': company.date,'primary': value,
-    'secondary': secondary_value,'primary_type': attribute, 'secondary_type': secondary_attribute}
+            'secondary': secondary_value,'primary_type': attribute, 'secondary_type': secondary_attribute}
 
     reply['text'] = card
     reply['type'] = 'company'
-    reply['speech'] = "The " + to_english[attribute] + " of " + company.name + " is " + value #The text to be spoken by the agent
+    reply['speech'] = "The " + to_english[attribute] + " of " + company.name + " is " + value
 
     return reply
 
+
 def sector_reply(sector, sector_attribute):
+    """
+    :param sector: A Sector whose data was requested
+    :param sector_attribute: The attribute that the response should show the value of
+    :return: A dictionary containing the layout of either a 'company card'/'big movers card'/'news card'
+    """
     data = getattr(sector, sector_attribute)
 
-    if (sector_attribute == "highest_price" or sector_attribute == "lowest_price"):
+    if sector_attribute == "highest_price" or sector_attribute == "lowest_price":
         data = getattr(sector, sector_attribute)
         sector_name = sector.name
         speech = "{} has the {} {} in {}: {}".format(data.name, sector_attribute.split('_',1)[0],
-                    sector_attribute.split('_', 1)[1], sector_name,
-                    getattr(data.stock, sector_attribute.split('_', 1)[1]))
+                                                     sector_attribute.split('_', 1)[1], sector_name,
+                                                     getattr(data.stock, sector_attribute.split('_', 1)[1]))
         response = get_company_reply(data, "price")
         response['speech'] = speech
         return response
@@ -200,6 +230,7 @@ def sector_reply(sector, sector_attribute):
             speech = "The following "+sector.name+" companies are "+sector_attribute+". "
         companies = []
 
+        # Build table of data
         for i in range(len(data)):
             row = defaultdict()
             row['name'] = data[i].name
@@ -242,13 +273,19 @@ def sector_reply(sector, sector_attribute):
         movers = defaultdict()
         movers['speech'] = "Here is some data about how "+sector.name+" are performing"
         card = defaultdict()
-        card['title'] = "Peformance of "+sector.name
+        card['title'] = "Performance of "+sector.name
         card['companies'] = companies
         movers['text'] = card
         movers['type'] = 'top'
         return movers
 
+
 def revenue_reply(company, date_period):
+    """
+    :param company: A Company whose revenue was requested
+    :param date_period: A string containing the date for which revenue data was requested for
+    :return: A dictionary containing the layout of the revenue card
+    """
     response = {}
 
     card = {}
@@ -285,8 +322,16 @@ def revenue_reply(company, date_period):
         response['type'] = 'error'
        
     return response
-  
+
+
 def daily_briefings(companies, sectors, attributes, days):
+    """
+    :param companies: A list of Company objects
+    :param sectors: A list of Sector objects
+    :param attributes: A list of strings that specify the attributes to display data for
+    :param days: An integer number of days of news to display
+    :return: A dictionary containing the layout of the cards to display
+    """
     response = {}
 
     if not companies and not sectors:
@@ -299,7 +344,7 @@ def daily_briefings(companies, sectors, attributes, days):
         briefing = defaultdict()
 
         company_attributes = {"current_price": "price", "daily_high": "high", "daily_low": "low",
-                             "percentage_change": "per_diff", "news": "news"}
+                              "percentage_change": "per_diff", "news": "news"}
 
         company_cards = []
         # Build company cards
